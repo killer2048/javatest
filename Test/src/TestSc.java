57903bd8bb738b41507232c3ed23bc36aed9be51
killer2048 <547474532@qq.com>
2017-12-26 20:02:46 +0800

    test add

---
+++ Test/src/TestSc.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class TestSc {
+	public static void main(String[] args) {
+		Scanner sc1 = new Scanner(System.in);
+		System.out.println(sc1.next());
+		Scanner sc2 = new Scanner(System.in);
+		System.out.println(sc2.next());
+	}
+}
